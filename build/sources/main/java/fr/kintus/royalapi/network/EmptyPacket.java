24006db8070214aef57d72b90cbe1d7fdddc975f
K1ntus <masson.jordan@gmail.com>
2020-08-13 10:13:42 +0200

    fixed Royalty custom forge events

---
+++ build/sources/main/java/fr/kintus/royalapi/network/EmptyPacket.java
@@ -0,0 +1,71 @@
+package fr.kintus.royalapi.network;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+public class EmptyPacket implements IMessage {
+	
+	    
+	    private String text;
+
+	    /**
+	     * Instantiates a new message to server.
+	     */
+	    public EmptyPacket() 
+	    { 
+	    	// need this constructor
+	    }
+
+	    /**
+	     * Instantiates a new message to server.
+	     *
+	     * @param parText the par text
+	     */
+	    public EmptyPacket(String parText) 
+	    {
+	        text = parText;
+	        // DEBUG
+	        System.out.println("MyMessage constructor");
+	    }
+
+	    /* (non-Javadoc)
+	     * @see net.minecraftforge.fml.common.network.simpleimpl.IMessage#fromBytes(io.netty.buffer.ByteBuf)
+	     */
+	    @Override
+	    public void fromBytes(ByteBuf buf) 
+	    {
+	        text = ByteBufUtils.readUTF8String(buf); // this class is very useful in general for writing more complex objects
+	    	// DEBUG
+	    	System.out.println("fromBytes = "+text);
+	    }
+
+	    /* (non-Javadoc)
+	     * @see net.minecraftforge.fml.common.network.simpleimpl.IMessage#toBytes(io.netty.buffer.ByteBuf)
+	     */
+	    @Override
+	    public void toBytes(ByteBuf buf) 
+	    {
+	        ByteBufUtils.writeUTF8String(buf, text);
+	        // DEBUG
+	        System.out.println("toBytes = "+text);
+	    }
+
+	    public static class Handler implements IMessageHandler<EmptyPacket, IMessage> 
+	    {
+	        
+			/* (non-Javadoc)
+			 * @see net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler#onMessage(net.minecraftforge.fml.common.network.simpleimpl.IMessage, net.minecraftforge.fml.common.network.simpleimpl.MessageContext)
+			 */
+			@Override
+			public IMessage onMessage(EmptyPacket message, MessageContext ctx) 
+			{
+	            System.out.println(String.format("Received %s from %s", message.text, ctx.getServerHandler().player));
+				return null;
+			}
+	    }
+	
+
+}
---
+++ build/sources/main/java/fr/kintus/royalapi/network/ModAPIPacket.java
@@ -0,0 +1,23 @@
+package fr.kintus.royalapi.network;
+
+import fr.kintus.royalapi.utils.ModUtils;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class ModAPIPacket {
+	public static final SimpleNetworkWrapper NETWORK_DISPATCHER = NetworkRegistry.INSTANCE.newSimpleChannel(ModUtils.MODID);
+
+	public static void init() {
+		registerPackets();
+	}
+	
+
+	private static void registerPackets() {
+        System.out.println("registering simple networking");
+
+        int packetId = 0;
+
+		NETWORK_DISPATCHER.registerMessage(EmptyPacket.Handler.class, EmptyPacket.class, packetId++, Side.SERVER);
+	}	
+}
