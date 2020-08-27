735fbea5624e328e5a33dd6ff95dd8cbdc9ab00a
K1ntus <masson.jordan@gmail.com>
2020-08-27 10:56:44 +0200

    improved git ignore and removed deprecated folders.

--- build/sources/main/java/fr/kintus/royalapi/utils/ModUtils.java
+++
@@ -1,8 +0,0 @@
-package fr.kintus.royalapi.utils;
-
-public class ModUtils {
-
-    public static final String MODID = "royalty_api";
-    public static final String NAME = "Royalty API";
-    public static final String VERSION = "1.0";
-}
